caeb5ae35b121331274c23ead7f654f59682c993
Eugene <gish.ee18@gmail.com>
2016-11-25 21:56:31 +0300

    Авторизация вынесена в метод

--- main.py
+++ main.py
@@ -2,22 +2,27 @@
 import vkrequests as vkr
 import os.path
 
-login = input('Логин: ')
-password = input('Пароль: ')
+def authorize():
+	if os.path.exists('data/token.txt'):
+		token = open('data/token.txt', 'r').read()
 
-if os.path.exists('data/token.txt'):
-	token = open('data/token.txt', 'r').read()
-	if vkr.log_in(token=token):
-		print(u'Успешная авторизация')
+		if vkr.log_in(token=token):
+			print(u'Успешная авторизация')
+		else:
+			print(u'Авторизация не удалась')
+
 	else:
-		print(u'Авторизация не удалась')
-else:
-	token = vkr.log_in(login=login, password=password)
-	if token:
-		print(u'Успешная авторизация')
-		open('data/token.txt', 'w').write(token)
-	else:
-		print(u'Авторизация не удалась')
+		login = input('Логин: ')
+		password = input('Пароль: ')
+		token = vkr.log_in(login=login, password=password)
+
+		if token:
+			print(u'Успешная авторизация')
+			open('data/token.txt', 'w').write(token)
+		else:
+			print(u'Авторизация не удалась')
+
+authorize()
 
 with open('data/message_dump.txt', 'w') as f:
 	pass

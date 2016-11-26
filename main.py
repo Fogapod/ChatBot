a79d02f1a261440d154a1bcd916e577220abd318
Eugene <gish.ee18@gmail.com>
2016-11-26 21:07:47 +0300

    Улучшено взаимодействие с пользователем, доба
    влена заготовка для разметки текста через ltk и тренировки нейронной
    сети tensorflow

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 import vkrequests as vkr
+import tensorflow as tf
+import numpy as np
 import os.path
+import random
+import pickle
+import nltk
 import time
+import re
 
-#import tensorflow as tf
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from collections import Counter
 
 class Profiler():
 	def __enter__(self):
@@ -22,25 +30,25 @@ def authorize():
 			print(u'Авторизация не удалась')
 
 	else:
-		login = input('Логин: ')
-		password = input('Пароль: ')
-		token = vkr.log_in(login=login, password=password)
+		while True:
+			login = input('Логин: ')
+			password = input('Пароль: ')
+			token = vkr.log_in(login=login, password=password)
 
-		if token:
-			print(u'Успешная авторизация')
-			open('data/token.txt', 'w').write(token)
-		else:
-			print(u'Авторизация не удалась')
+			if token:
+				print(u'Успешная авторизация')
+				open('data/token.txt', 'w').write(token)
+				break
+			else:
+				print(u'Авторизация не удалась')
 
 	global SELF_ID
 	SELF_ID = vkr.get_user_id()
 
-authorize()
-
 #vkr.FRIEND_ID = vkr.get_user_id(link=input(u'Короткая ссылка на страницу друга: '))
 #print(vkr.FRIEND_ID)
 
-with open('data/message_dump.txt', 'a+') as f:
+def message_getter(f):
 	messages_list = vkr.get_messages_list()
 	print(u'Обнаружено {} диалогов'.format(messages_list['count']))
 	for k in range(messages_list['count']//200 + 1):
@@ -55,22 +63,24 @@ with open('data/message_dump.txt', 'a+') as f:
 				print(u'Сообщения из беседы, пропускаю')
 				continue
 			else:
-				print(
-					u'Сообщений в диалоге: {}::{}'.format(\
-						messages['count'],
-						vkr.get_user_name(
+				uname = vkr.get_user_name(
 							uid=messages_list['items'][d]['user_id']
 							)
+				print(u'Сообщений в диалоге: {}::{}'.format(\
+						messages['count'], uname
+							)
 						)
-					)
 
 			for i in range(messages['count']//200 + 1):
 				for j in range(len(messages['items'])):
 					if messages['items'][j]['body'] is not '':
 						try:
 							f.write(
-								'{} {}\n'.format(\
-								'You' if messages['items'][j]['from_id'] == SELF_ID else 'Friend',
+							'{} {}\n'.format(\
+							'You' if messages['items'][j]['from_id'] ==\
+								SELF_ID else 'Friend::{}::{}'.format(\
+									messages['items'][j]['from_id'], uname
+									),
 								messages['items'][j]['body']
 								)
 							)
@@ -85,4 +95,85 @@ with open('data/message_dump.txt', 'a+') as f:
 
 			print(u'Завершена обработка диалога №{}'.format((k)*200 + d+1))
 		messages_list = vkr.get_messages_list(offset=(k+1)*200)
-		
+
+authorize()
+if os.path.exists('data/message_dump.txt'):
+	while True:
+		ans = input(u'Файл с историей сообщений уже существует. Заменить его? (y/n) ')
+		if ans.lower() == 'y':
+			with open('data/message_dump.txt', 'a+') as f:
+				f.seek(0)
+				f.truncate()
+				message_getter(f)
+				break
+		elif ans.lower() == 'n':
+			break
+		else:
+			print(u'Неизвестный ответ.')
+
+lemmatizer = WordNetLemmatizer()
+
+def create_lexicon(path):
+
+	lexicon = []
+	with open(path,'r') as f:
+		contents = f.readlines()
+		for line in contents:
+			line = re.sub(r'(^.*)|(^.* .*)', '', line)
+			all_words = word_tokenize(line)
+			lexicon += list(all_words)
+
+	lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
+	w_counts = Counter(lexicon)
+	l2 = []
+	for w in w_counts:
+		if 1000 > w_counts[w] > 4:
+			l2.append(w)
+
+	return l2
+
+
+def sample_handling(sample,lexicon,classification):
+
+	featureset = []
+
+	with open(sample,'r') as f:
+		contents = f.readlines()
+		for l in contents:
+			current_words = word_tokenize(l.lower())
+			current_words = [lemmatizer.lemmatize(i) for i in current_words]
+			features = np.zeros(len(lexicon))
+			for word in current_words:
+				if word.lower() in lexicon:
+					index_value = lexicon.index(word.lower())
+					features[index_value] += 1
+
+			features = list(features)
+			featureset.append([features,classification])
+
+	return featureset
+
+
+def create_feature_sets_and_labels(path,test_size = 0.1):
+	lexicon = create_lexicon(path)
+	features = []
+	features += sample_handling('data/message_dump.txt',lexicon,[1])
+	random.shuffle(features)
+	features = np.array(features)
+
+	testing_size = int(test_size*len(features))
+
+	train_x = list(features[:,0][:-testing_size])
+	train_y = list(features[:,1][:-testing_size])
+	test_x = list(features[:,0][-testing_size:])
+	test_y = list(features[:,1][-testing_size:])
+
+	return train_x,train_y,test_x,test_y
+
+'''
+if __name__ == '__main__':
+	train_x,train_y,test_x,test_y = create_feature_sets_and_labels('data/message_dump.txt')
+	# if you want to pickle this data:
+	with open('data/sentiment_set.pickle','wb') as f:
+		pickle.dump([train_x,train_y,test_x,test_y],f)
+'''

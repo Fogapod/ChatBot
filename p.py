45586a7dceb17fb9c7df4ad5d9e1d89345ed501f
Eugene <gish.ee18@gmail.com>
2016-11-30 14:47:12 +0300

    Работающий бот с базовыми функциями

--- p.py
+++ p.py
@@ -1,30 +1,87 @@
-import pycurl, json
-
-STREAM_URL = "https://imv4.vk.com/im2898?act=a_check&key=2522b126a2014fcd5baa251991faf371a57d5a6c&ts=1671810638&version=1"
-STREAM_URL = 'https://api.vk.com/method/{}?ts={}&pts={}&fields={}&access_token={}&v={}'.format(\
-'messages.getLongPollHistory',
-'1671810801',
-'1644309',
-'sex',
-'6cedb48d283f5b33c9e0b1cc7d6257e4612b4e17838ed68ad1927aa64023fc3f518bcdc48db2a359db8ee',
-'5.60'
-)
+import pycurl
+import json
+import os
+import vkrequests as vkr
+import re
+import asyncio
 
 class Client:
   def __init__(self):
-    self.buffer = ""
     self.conn = pycurl.Curl()
-    self.conn.setopt(pycurl.URL, STREAM_URL)
+    self.STREAM_URL = 'https://{}?act={}&key={}&ts={}&wait={}&mode={}&version={}'
+    self.data = None
+    self.SELF_ID = None
+
+  def authorize(self):
+    if os.path.exists('data/token.txt'):
+      token = open('data/token.txt', 'r').read()
+      if token:
+        if vkr.log_in(token=token):
+          print('Успешная авторизация')
+        else:
+          print('Авторизация не удалась')
+
+      else:
+        while True:
+          login = input('Логин: ')
+          password = input('Пароль: ')
+          new_token = vkr.log_in(login=login, password=password)
+          if new_token:
+            print('Успешная авторизация')
+            open('data/token.txt', 'w').write(new_token)
+            break
+          else:
+            print('Авторизация не удалась')
+    self.SELF_ID = vkr.get_user_id()
+
+  def set_url(self, server, key, ts, act='a_check', wait=30, mode=128, v=1):
+    self.url = self.STREAM_URL.format(server, act, key, ts, wait, mode, v)
+    return self.url
+
+  def navigate(self, url):
+    self.conn.setopt(pycurl.URL, url)
     self.conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
     self.conn.perform()
+    return self.data
 
   def on_receive(self, data):
     data = data.decode('utf-8')
-    try:
-      data = json.loads(data)['response']
-      print(data)
-      print(data['new_pts'])
-    except:
-      print(data)
+    data = json.loads(data)
+    self.data = data
 
 client = Client()
+last_rnd_id = 0
+client.authorize()
+lpd = vkr.get_long_poll_data()
+url = client.set_url(
+lpd['server'],
+lpd['key'],
+lpd['ts']
+)
+while True:
+  response = client.navigate(url)
+  print(response)
+  url = client.set_url(
+    lpd['server'],
+    lpd['key'],
+    response['ts']
+  )
+  for update in response['updates']:
+      if update[0] is 4 and update[7] != last_rnd_id and update[3]:
+        text = update[6]
+        if text.lower() == 'ершов' or text.lower() == 'женя' or text.lower() == 'жень':
+          text = 'А'
+        elif re.sub('^( )*', '', text).startswith('/'):
+          text = text[1:]
+          if re.match('^скажи ', text.lower()):
+            text = re.findall('^С?с?кажи (.+)/?', text)[0]
+          else:
+            text = 'Попка молодец🐔'
+        else:
+          continue
+        vkr.send_message(
+          uid=update[3],
+          text=text + "'",
+          rnd_id=update[7]+1
+        )
+        last_rnd_id = update[7]+1

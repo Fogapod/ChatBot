924a12e2e2521907363ceb6bf508dc381be5624a
Eugene <gish.ee18@gmail.com>
2016-12-05 21:40:34 +0300

    Улучшение модуля parse_input

--- utils.py
+++ utils.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import re
-def parse_input(string):
-	re.sub(r'(https?://)?(m\.)?vk\.com/.*$',
-		'__vkurl__',
-		string.lower() # поиск ссылок vk.com
-	)
+def parse_input(string, replace_vkurl=True, replace_url=True, replace_nl=True):
+	new_string = string
 
-	string = re.sub(
-		r'''(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]+|(([^\s()<>]+)))*)|[^\s`!()[]{};:'".,<>?«»“”‘’]))'''
+	if replace_vkurl:
+		new_string = re.sub(r'(https?://)?m|\.?vk\.com/?.*',
+			'__vkurl__',
+			new_string.lower() # поиск ссылок vk.com
+		)
+
+	if replace_url:
+		new_string = re.sub(
+		r'''(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/?)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]+|(([^\s()<>]+)))*)|[^\s`!()[]{};:'".,<>?«»“”‘’]))'''
 		, '__url__', # поиск всех остальных ссылок
-		string
+		new_string
 	)
 
-	string = re.sub('\n', '__nl__', string) # поиск переносов на новую строку
-	return string
